158c1fb12e753588af30920ba423672dfcce9b9a
Taylor Somers <tsomers@yandex.com>
2020-07-12 16:48:05 -0400

    Add logic to double inputted user number and display the result

--- DoubleIt.cs
+++ DoubleIt.cs
@@ -6,5 +6,7 @@ class DoubleIt
   {
     Console.WriteLine("Give me a number and I will double it for you!");
     int yourNumber = Console.ReadLine();
+    int yourDoubledNumber = yourNumber * 2;
+    Console.WriteLine("I doubled your number for you: " + yourDoubledNumber);
   }
 }
