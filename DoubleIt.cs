1a51be084e376d1e0a12b6bd24baccfb7f5e4b27
Taylor Somers <tsomers@yandex.com>
2020-07-12 16:52:43 -0400

    Fix data type error

--- DoubleIt.cs
+++ DoubleIt.cs
@@ -5,8 +5,10 @@ class DoubleIt
   static void Main()
   {
     Console.WriteLine("Give me a number and I will double it for you!");
-    int yourNumber = Console.ReadLine();
+    string stringNumber = Console.ReadLine();
+    int yourNumber = int.Parse(stringNumber);
     int yourDoubledNumber = yourNumber * 2;
+    string stringDoubledNumber = yourDoubledNumber.ToString();
     Console.WriteLine("I doubled your number for you: " + yourDoubledNumber);
   }
 }
